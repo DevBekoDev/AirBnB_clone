38890a9cb6f5e526b382efd421e8f6252ec4c6b7
HMFathi10 <hossamfci10@gmail.com>
2024-01-13 18:06:03 +0200

    Added

---
+++ console.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+class HBNBCommand(cmd.Cmd):
+     """Defines the HolbertonBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+    prompt = "(hbnb) "
+
+    def to_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def to_EOF(self, arg):
+        """EOF signal to exit the prgram."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+    def do_create(self, arg):
+        """Usage: create <class>
+        Create a new class instance and print its id.
+        """
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] is not BaseModel:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg[0])().id)
+            storage.save()
+
+    def do_show(self, arg):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
+        """
+        objdict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.BaseModel:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in objdict:
+            print("** no instance found **")
+        else:
+            print(objdict["{}.{}".format(arg[0], arg[1])])
+
+    def do_destroy(self, arg):
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id."""
+        objdict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in BaseModel:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in objdict.keys():
+            print("** no instance found **")
+        else:
+            del objdict["{}.{}".format(arg[0], arg[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """Usage: all or all <class> or <class>.all()
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects."""
+        if len(arg) > 0 and arg[0] not in BaseModel:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in storage.all().values():
+                if len(arg) > 0 and arg[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(arg) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
+
+    def do_update(self, arg):
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        objdict = storage.all()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return False
+        if arg[0] not in BaseModel:
+            print("** class doesn't exist **")
+            return False
+        if len(arg) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(arg[0], arg[1]) not in objdict.keys():
+            print("** no instance found **")
+            return False
+        if len(arg) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(arg) == 3:
+            try:
+                type(eval(arg[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(arg) == 4:
+            obj = objdict["{}.{}".format(arg[0], arg[1])]
+            if arg[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[arg[2]])
+                obj.__dict__[arg[2]] = valtype(arg[3])
+            else:
+                obj.__dict__[arg[2]] = arg[3]
+        elif type(eval(arg[2])) == dict:
+            obj = objdict["{}.{}".format(arg[0], arg[1])]
+            for k, v in eval(arg[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
+
+    if __name__ == "__main__":
+        HBNBCommand().cmdloop()
